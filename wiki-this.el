cae70b97c9df870da5013076c3c673fabffa157e
jenchieh <jcs090218@gmail.com>
2018-12-07 01:07:20 +0800

    Add core.

---
+++ wiki-this.el
@@ -0,0 +1,47 @@
+;;; wiki-this.el --- Simply search in Wikipedia under point.                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Shen, Jen-Chieh
+;; Created date 2018-12-07 00:42:51
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Simply search in Wikipedia under point.
+;; Keyword: search wiki wikipedia
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.4"))
+;; URL: https://github.com/jcs090218/wiki-this
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Simply search in Wikipedia under point.
+;;
+
+;;; Code:
+
+(defgroup wiki-this nil
+  "Simply search in Wikipedia under point"
+  :prefix "wiki-this-"
+  :group 'tool
+  :link '(url-link :tag "Repository" "https://github.com/jcs090218/wiki-this"))
+
+
+;;;###autoload
+(defun wiki-this ()
+  "")
+
+(provide 'wiki-this)
+;;; wiki-this.el ends here
